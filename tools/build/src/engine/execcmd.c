4421d87b6c5cdcf4882fc1e147d07e0d623f8396
张文 <jimokanghanchao@gmail.com>
2023-04-15 20:14:57 +0800

    fix: implicit function

--- tools/build/src/engine/execcmd.c
+++ tools/build/src/engine/execcmd.c
@@ -10,15 +10,14 @@
 
 #include "jam.h"
 #include "execcmd.h"
+#include "output.h"
 
 #include <assert.h>
 #include <stdio.h>
 #include <string.h>
 
-
 /* Internal interrupt counter. */
 static int intr;
-
 
 /* Constructs a list of command-line elements using the format specified by the
  * given shell list.
@@ -39,83 +38,85 @@ static int intr;
  *   - If no '%' element is found, the command string is appended as an extra.
  */
 
-void argv_from_shell( char const * * argv, LIST * shell, char const * command,
-    int const slot )
+void argv_from_shell(char const **argv, LIST *shell, char const *command,
+                     int const slot)
 {
-    static char jobno[ 4 ];
+    static char jobno[4];
 
     int i;
     int gotpercent = 0;
-    LISTITER iter = list_begin( shell );
-    LISTITER end = list_end( shell );
+    LISTITER iter = list_begin(shell);
+    LISTITER end = list_end(shell);
 
-    assert( 0 <= slot );
-    assert( slot < 999 );
-    sprintf( jobno, "%d", slot + 1 );
+    assert(0 <= slot);
+    assert(slot < 999);
+    sprintf(jobno, "%d", slot + 1);
 
-    for ( i = 0; iter != end && i < MAXARGC; ++i, iter = list_next( iter ) )
+    for (i = 0; iter != end && i < MAXARGC; ++i, iter = list_next(iter))
     {
-        switch ( object_str( list_item( iter ) )[ 0 ] )
+        switch (object_str(list_item(iter))[0])
         {
-            case '%': argv[ i ] = command; ++gotpercent; break;
-            case '!': argv[ i ] = jobno; break;
-            default : argv[ i ] = object_str( list_item( iter ) );
+        case '%':
+            argv[i] = command;
+            ++gotpercent;
+            break;
+        case '!':
+            argv[i] = jobno;
+            break;
+        default:
+            argv[i] = object_str(list_item(iter));
         }
     }
 
-    if ( !gotpercent )
-        argv[ i++ ] = command;
+    if (!gotpercent)
+        argv[i++] = command;
 
-    argv[ i ] = NULL;
+    argv[i] = NULL;
 }
-
 
 /* Returns whether the given command string contains lines longer than the given
  * maximum.
  */
-int check_cmd_for_too_long_lines( char const * command, int const max,
-    int * const error_length, int * const error_max_length )
+int check_cmd_for_too_long_lines(char const *command, int const max,
+                                 int *const error_length, int *const error_max_length)
 {
-    while ( *command )
+    while (*command)
     {
-        size_t const l = strcspn( command, "\n" );
-        if ( l > max )
+        size_t const l = strcspn(command, "\n");
+        if (l > max)
         {
             *error_length = l;
             *error_max_length = max;
             return EXEC_CHECK_LINE_TOO_LONG;
         }
         command += l;
-        if ( *command )
+        if (*command)
             ++command;
     }
     return EXEC_CHECK_OK;
 }
 
-
 /* Checks whether the given shell list is actually a request to execute raw
  * commands without an external shell.
  */
-int is_raw_command_request( LIST * shell )
+int is_raw_command_request(LIST *shell)
 {
-    return !list_empty( shell ) &&
-        !strcmp( object_str( list_front( shell ) ), "%" ) &&
-        list_next( list_begin( shell ) ) == list_end( shell );
+    return !list_empty(shell) &&
+           !strcmp(object_str(list_front(shell)), "%") &&
+           list_next(list_begin(shell)) == list_end(shell);
 }
-
 
 /* Returns whether an interrupt has been detected so far. */
 
-int interrupted( void )
+int interrupted(void)
 {
     return intr != 0;
 }
 
-
 /* Internal interrupt handler. */
 
-void onintr( int disp )
+void onintr(int disp)
 {
     ++intr;
-    out_printf( "...interrupted\n" );
+    out_printf("...interrupted\n");
 }
--- tools/build/src/engine/modules/path.c
+++ tools/build/src/engine/modules/path.c
@@ -9,17 +9,15 @@
 #include "../lists.h"
 #include "../native.h"
 #include "../timestamp.h"
+#include "../filesys.h"
 
-
-LIST * path_exists( FRAME * frame, int flags )
+LIST *path_exists(FRAME *frame, int flags)
 {
-    return file_query( list_front( lol_get( frame->args, 0 ) ) ) ?
-        list_new( object_copy( constant_true ) ) : L0;
+    return file_query(list_front(lol_get(frame->args, 0))) ? list_new(object_copy(constant_true)) : L0;
 }
-
 
 void init_path()
 {
-    char const * args[] = { "location", 0 };
-    declare_native_rule( "path", "exists", args, path_exists, 1 );
+    char const *args[] = {"location", 0};
+    declare_native_rule("path", "exists", args, path_exists, 1);
 }
